69859a9ccfd16b9be02228ef3cf71f738cb00cb5
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-08-01 16:39:49 +0530

    Add requirements file and configure environment variables
    - Added [`requirements.txt`](command:_github.copilot.openRelativePath?%5B%7B%22scheme%22%3A%22file%22%2C%22authority%22%3A%22%22%2C%22path%22%3A%22%2FUsers%2Fapple%2FDocuments%2Fscorm%2Frequirements.txt%22%2C%22query%22%3A%22%22%2C%22fragment%22%3A%22%22%7D%5D "/Users/apple/Documents/scorm/requirements.txt") to manage project dependencies.
    - Included `python-dotenv` to handle environment variables.
    - Updated [`wsgi.py`](command:_github.copilot.openRelativePath?%5B%7B%22scheme%22%3A%22file%22%2C%22authority%22%3A%22%22%2C%22path%22%3A%22%2FUsers%2Fapple%2FDocuments%2Fscorm%2Fscorm_player%2Fwsgi.py%22%2C%22query%22%3A%22%22%2C%22fragment%22%3A%22%22%7D%5D "/Users/apple/Documents/scorm/scorm_player/wsgi.py") to load environment variables using [`dotenv`](command:_github.copilot.openSymbolFromReferences?%5B%7B%22%24mid%22%3A1%2C%22path%22%3A%22%2FUsers%2Fapple%2FDocuments%2Fscorm%2Fenv%2Flib%2Fpython3.12%2Fsite-packages%2Fdotenv%2F__init__.py%22%2C%22scheme%22%3A%22file%22%7D%2C%7B%22line%22%3A0%2C%22character%22%3A0%7D%5D "env/lib/python3.12/site-packages/dotenv/__init__.py").

--- scorm_player/settings.py
+++ scorm_player/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f)sk$s7sp6!31m0qqgbmrwueq4gpowkw4arrd3c5yi^(i6_hfa'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
