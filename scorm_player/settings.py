1188d3e4ebd00ffd87c88043d94d65809aeeda77
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-08-20 13:58:31 +0530

    update: STATIC_ROOT

--- scorm_player/settings.py
+++ scorm_player/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.getenv('STATIC_ROOT')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
